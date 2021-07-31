f7f69e8b55219df235bf5b325bc1d476175b2320
Brian Park <brian@xparks.net>
2021-07-30 17:55:08 -0700

    docs: Regenerate doxygen docs for v0.2

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['sendbyte_3',['sendByte',['../classace__tmi_1_1SoftTmiInterface.html#ab78e3ed01e06a8b3e29490b90b5e5cdf',1,'ace_tmi::SoftTmiInterface']]],
-  ['softtmiinterface_4',['SoftTmiInterface',['../classace__tmi_1_1SoftTmiInterface.html',1,'ace_tmi::SoftTmiInterface'],['../classace__tmi_1_1SoftTmiInterface.html#a45f5d3545f184cc9f64e7782e794fa6c',1,'ace_tmi::SoftTmiInterface::SoftTmiInterface()']]],
-  ['startcondition_5',['startCondition',['../classace__tmi_1_1SoftTmiInterface.html#a4a4b1f2a3636c33ad0d724f0ed89e376',1,'ace_tmi::SoftTmiInterface']]],
-  ['stopcondition_6',['stopCondition',['../classace__tmi_1_1SoftTmiInterface.html#a9b4e027890a3c8874f21325f70317362',1,'ace_tmi::SoftTmiInterface']]]
+  ['sendbyte_3',['sendByte',['../classace__tmi_1_1SimpleTmiFastInterface.html#aec2d70b9d434bd18f73e570878109352',1,'ace_tmi::SimpleTmiFastInterface::sendByte()'],['../classace__tmi_1_1SimpleTmiInterface.html#a534ae23a031c028e24dd64540f50783d',1,'ace_tmi::SimpleTmiInterface::sendByte()']]],
+  ['simpletmifastinterface_4',['SimpleTmiFastInterface',['../classace__tmi_1_1SimpleTmiFastInterface.html',1,'ace_tmi::SimpleTmiFastInterface&lt; T_DIO_PIN, T_CLK_PIN, T_DELAY_MICROS &gt;'],['../classace__tmi_1_1SimpleTmiFastInterface.html#a59d6773b94d1e127d7c7651efe48d39a',1,'ace_tmi::SimpleTmiFastInterface::SimpleTmiFastInterface()']]],
+  ['simpletmiinterface_5',['SimpleTmiInterface',['../classace__tmi_1_1SimpleTmiInterface.html',1,'ace_tmi::SimpleTmiInterface'],['../classace__tmi_1_1SimpleTmiInterface.html#a8082581169f1434f614bc359c1423297',1,'ace_tmi::SimpleTmiInterface::SimpleTmiInterface()']]],
+  ['startcondition_6',['startCondition',['../classace__tmi_1_1SimpleTmiFastInterface.html#afc60e6299e2a9906150f12a08a78c0f6',1,'ace_tmi::SimpleTmiFastInterface::startCondition()'],['../classace__tmi_1_1SimpleTmiInterface.html#a587812128aee98f03720940ddddc744f',1,'ace_tmi::SimpleTmiInterface::startCondition()']]],
+  ['stopcondition_7',['stopCondition',['../classace__tmi_1_1SimpleTmiFastInterface.html#a16b97f7f7dc1abbb865e33436e674147',1,'ace_tmi::SimpleTmiFastInterface::stopCondition()'],['../classace__tmi_1_1SimpleTmiInterface.html#ae745d33c115acf4879f426f4991a0cd7',1,'ace_tmi::SimpleTmiInterface::stopCondition()']]]
 ];
--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin_8',['begin',['../classace__tmi_1_1SoftTmiInterface.html#af2bb5a5f0b21baf4d9c32fce8df02fde',1,'ace_tmi::SoftTmiInterface']]]
+  ['begin_10',['begin',['../classace__tmi_1_1SimpleTmiFastInterface.html#a988aa1e7815f234a171b8bce82ee945d',1,'ace_tmi::SimpleTmiFastInterface::begin()'],['../classace__tmi_1_1SimpleTmiInterface.html#a258ea205516c6b9d750c0e051975dbed',1,'ace_tmi::SimpleTmiInterface::begin()']]]
 ];
