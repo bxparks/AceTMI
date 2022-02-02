1445d01b590f21c774d25ad06617f7908b525738
Brian Park <brian@xparks.net>
2022-02-02 11:48:02 -0800

    docs: Regenerate doxygen

--- docs/html/search/classes_0.js
+++ docs/html/search/classes_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['simpletmi1638fastinterface_12',['SimpleTmi1638FastInterface',['../classace__tmi_1_1SimpleTmi1638FastInterface.html',1,'ace_tmi']]],
-  ['simpletmi1638interface_13',['SimpleTmi1638Interface',['../classace__tmi_1_1SimpleTmi1638Interface.html',1,'ace_tmi']]],
-  ['simpletmifastinterface_14',['SimpleTmiFastInterface',['../classace__tmi_1_1SimpleTmiFastInterface.html',1,'ace_tmi']]],
-  ['simpletmiinterface_15',['SimpleTmiInterface',['../classace__tmi_1_1SimpleTmiInterface.html',1,'ace_tmi']]]
+  ['simpletmi1637fastinterface_12',['SimpleTmi1637FastInterface',['../classace__tmi_1_1SimpleTmi1637FastInterface.html',1,'ace_tmi']]],
+  ['simpletmi1637interface_13',['SimpleTmi1637Interface',['../classace__tmi_1_1SimpleTmi1637Interface.html',1,'ace_tmi']]],
+  ['simpletmi1638fastinterface_14',['SimpleTmi1638FastInterface',['../classace__tmi_1_1SimpleTmi1638FastInterface.html',1,'ace_tmi']]],
+  ['simpletmi1638interface_15',['SimpleTmi1638Interface',['../classace__tmi_1_1SimpleTmi1638Interface.html',1,'ace_tmi']]]
 ];
--- docs/html/search/functions_2.js
+++ docs/html/search/functions_2.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['simpletmi1638fastinterface_20',['SimpleTmi1638FastInterface',['../classace__tmi_1_1SimpleTmi1638FastInterface.html#a2171425ee0a5385bc2b6481da8420a5f',1,'ace_tmi::SimpleTmi1638FastInterface']]],
-  ['simpletmi1638interface_21',['SimpleTmi1638Interface',['../classace__tmi_1_1SimpleTmi1638Interface.html#a1472671b5260600a804fd8258abef7b2',1,'ace_tmi::SimpleTmi1638Interface']]],
-  ['simpletmifastinterface_22',['SimpleTmiFastInterface',['../classace__tmi_1_1SimpleTmiFastInterface.html#a59d6773b94d1e127d7c7651efe48d39a',1,'ace_tmi::SimpleTmiFastInterface']]],
-  ['simpletmiinterface_23',['SimpleTmiInterface',['../classace__tmi_1_1SimpleTmiInterface.html#a8082581169f1434f614bc359c1423297',1,'ace_tmi::SimpleTmiInterface']]],
-  ['startcondition_24',['startCondition',['../classace__tmi_1_1SimpleTmiFastInterface.html#afc60e6299e2a9906150f12a08a78c0f6',1,'ace_tmi::SimpleTmiFastInterface::startCondition()'],['../classace__tmi_1_1SimpleTmiInterface.html#a587812128aee98f03720940ddddc744f',1,'ace_tmi::SimpleTmiInterface::startCondition()']]],
-  ['stopcondition_25',['stopCondition',['../classace__tmi_1_1SimpleTmiFastInterface.html#a16b97f7f7dc1abbb865e33436e674147',1,'ace_tmi::SimpleTmiFastInterface::stopCondition()'],['../classace__tmi_1_1SimpleTmiInterface.html#ae745d33c115acf4879f426f4991a0cd7',1,'ace_tmi::SimpleTmiInterface::stopCondition()']]]
+  ['simpletmi1637fastinterface_20',['SimpleTmi1637FastInterface',['../classace__tmi_1_1SimpleTmi1637FastInterface.html#a840afa3890f6d46db60c683379eaa975',1,'ace_tmi::SimpleTmi1637FastInterface']]],
+  ['simpletmi1637interface_21',['SimpleTmi1637Interface',['../classace__tmi_1_1SimpleTmi1637Interface.html#aff4414cae78bd56a520ab1191a7e2ed4',1,'ace_tmi::SimpleTmi1637Interface']]],
+  ['simpletmi1638fastinterface_22',['SimpleTmi1638FastInterface',['../classace__tmi_1_1SimpleTmi1638FastInterface.html#a2171425ee0a5385bc2b6481da8420a5f',1,'ace_tmi::SimpleTmi1638FastInterface']]],
+  ['simpletmi1638interface_23',['SimpleTmi1638Interface',['../classace__tmi_1_1SimpleTmi1638Interface.html#a1472671b5260600a804fd8258abef7b2',1,'ace_tmi::SimpleTmi1638Interface']]],
+  ['startcondition_24',['startCondition',['../classace__tmi_1_1SimpleTmi1637FastInterface.html#a2e14be9ce0de35300ae1ca4b37d39d32',1,'ace_tmi::SimpleTmi1637FastInterface::startCondition()'],['../classace__tmi_1_1SimpleTmi1637Interface.html#ad5f638f23cea898773f676a578167c8b',1,'ace_tmi::SimpleTmi1637Interface::startCondition()']]],
+  ['stopcondition_25',['stopCondition',['../classace__tmi_1_1SimpleTmi1637FastInterface.html#a940315894e12d041e837ae360ce6a827',1,'ace_tmi::SimpleTmi1637FastInterface::stopCondition()'],['../classace__tmi_1_1SimpleTmi1637Interface.html#a1735ef0cd9df86fe54d516eb10b8cc1a',1,'ace_tmi::SimpleTmi1637Interface::stopCondition()']]]
 ];
